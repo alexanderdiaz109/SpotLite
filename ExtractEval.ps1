867d04ff4d5ffd4625dff6c9009d555d25588678
RetroWave2k <alejandro.solis.0809gmail.com>
2026-02-24 12:22:56 -0600

    borrando basura

--- ExtractEval.ps1
+++
@@ -1,4 +0,0 @@
-
-$evals = Invoke-RestMethod -Uri "https://spotlight-api-m2kt.onrender.com/api/Evaluations" -Method Get
-$evals[-1] | ConvertTo-Json -Depth 5
-
--- check.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const data = JSON.parse(fs.readFileSync('output.txt', 'utf8').split('Latest evaluation:\r\n')[1]); console.log(JSON.stringify(data, null, 2));
